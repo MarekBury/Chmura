414eafc2de68dddca706ebc546e6d7a63bc93ae9
Marek <marekbury94@gmail.com>
2020-12-06 19:11:52 +0100

    Debug off

--- Chmura/Chmura/settings.py
+++ Chmura/Chmura/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '$d&wlvu#b%z@p0%h+@t4m1w5k487m-wayf3=ocf+g=6$!b$#d7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
